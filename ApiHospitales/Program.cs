e9386ea59f3cff59bc0531d62a1d3b6c634a08c5
mmtol <mmigueltolosana@gmail.com>
2026-03-25 09:26:40 +0100

    aaa

--- ApiHospitales/Program.cs
+++ ApiHospitales/Program.cs
@@ -1,6 +1,7 @@
 using ApiHospitales.Data;
 using ApiHospitales.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,10 +18,11 @@ builder.Services.AddDbContext<HospitalContext>(options => options.UseSqlServer(c
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
